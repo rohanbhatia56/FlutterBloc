d9ae432e522037bf730f9db46f5130c4489b4017
rohanbhatia56 <bhatiarohan156@gmail.com>
2025-03-31 13:56:17 +0530

    added flight factory implementations

---
+++ lib/flight/factory/product/implemenation/DomesticFlight.dart
@@ -0,0 +1,8 @@
+
+// Concrete class for Domestic Flight
+class DomesticFlight extends FlightBooking {
+  @override
+  void bookFlight() {
+    print("Domestic flight booked successfully!");
+  }
+}
---
+++ lib/flight/factory/product/implemenation/InternationalFlight.dart
@@ -0,0 +1,7 @@
+// Concrete class for International Flight
+class InternationalFlight extends FlightBooking {
+  @override
+  void bookFlight() {
+    print("International flight booked successfully!");
+  }
+}
